6b72530d87262978f2ff3c67af18cd86b90fec3e
haalmarc <marcus-haa@hotmail.com>
2019-11-09 17:11:45 +0100

    Updates

---
+++ precache-manifest.d3b7f11d500238d0b61352db24330cff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a1de8957d1aa9869ee26f688357ba8",
+    "url": "/cv.git/index.html"
+  },
+  {
+    "revision": "6b2dd0efbffb4dea69c0",
+    "url": "/cv.git/static/css/main.4f6252a2.chunk.css"
+  },
+  {
+    "revision": "e29199eecf0de8e41c6c",
+    "url": "/cv.git/static/js/2.2ec6d98f.chunk.js"
+  },
+  {
+    "revision": "6b2dd0efbffb4dea69c0",
+    "url": "/cv.git/static/js/main.82e34788.chunk.js"
+  },
+  {
+    "revision": "2632fb30704046e37154",
+    "url": "/cv.git/static/js/runtime-main.5fbbdd4a.js"
+  }
+]);
