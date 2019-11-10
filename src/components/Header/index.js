2dfcc9d46d79fdf5dab1118d8525d8befa65f891
haalmarc <marcus-haa@hotmail.com>
2019-11-10 02:44:54 +0100

    Wanting to add animated menu icon, fix parameter

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './index.css';
+import Hamburgermenu from '../Hamburgermenu';
 
 const Header = () => (
   <header>
     <i className="fa fa-bars faIcon"></i>
+    <Hamburgermenu />
   </header>
 );
 
