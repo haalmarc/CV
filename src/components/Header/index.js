9f6b2c9ef03f42513e8573467d9893a5b1c097e5
haalmarc <marcus-haa@hotmail.com>
2019-11-13 01:49:12 +0100

    Add logic for hamburgermenu. Lacking nav-design

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './index.css';
 
-const Header = () => (
-  <header>
-    <nav>
-      <a href="#utdanning" className="navElement">Utdanning</a>
-      <a href="#erfaring" className="navElement">Erfaring</a>
-      <a href="#footer" className="navElement">Kontakt</a>
-    </nav>
-    {/* Add when hamburger functions <i className="fa fa-bars faIcon"></i> */}
-  </header>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hamburger: "inactive" }
+  }
+
+  /* Veksle mellom states når knapp trykkes */
+  toggleMenu = () => {
+    if (this.state.hamburger === "inactive") {
+      this.setState({hamburger: "active"})
+    } if (this.state.hamburger === "active") {
+      this.setState({hamburger: "inactive"})
+    }
+  }
+
+
+  render () {
+    return (
+      <header>
+        <nav className={this.state.hamburger}>
+          <a href="#utdanning" className="navElement">Utdanning</a>
+          <a href="#erfaring" className="navElement">Erfaring</a>
+          <a href="#footer" className="navElement">Kontakt</a>
+        </nav>
+        <button onClick={this.toggleMenu}><i className="fa fa-bars faIcon"></i></button>
+      </header>
+    )
+  }
+} 
 
 export default Header;
