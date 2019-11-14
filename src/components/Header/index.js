743cd1bc9c09e4dafe61644e3cff81a92b8c0c1f
haalmarc <marcus-haa@hotmail.com>
2019-11-14 21:35:30 +0100

    Remove navbar code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,30 +2,14 @@ import React from 'react';
 import './index.css';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hamburger: "inactive" }
-  }
-
-  /* Veksle mellom states når knapp trykkes */
-  toggleMenu = () => {
-    if (this.state.hamburger === "inactive") {
-      this.setState({hamburger: "active"})
-    } if (this.state.hamburger === "active") {
-      this.setState({hamburger: "inactive"})
-    }
-  }
-
-
   render () {
     return (
       <header>
-        <nav className={this.state.hamburger}>
+        <nav>
           <a href="#utdanning" className="navElement">Utdanning</a>
           <a href="#erfaring" className="navElement">Erfaring</a>
           <a href="#footer" className="navElement">Kontakt</a>
         </nav>
-        <button onClick={this.toggleMenu}><i className="fa fa-bars faIcon"></i></button>
       </header>
     )
   }
