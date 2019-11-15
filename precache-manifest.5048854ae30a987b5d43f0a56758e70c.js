37e7d8098ac6cb6cbe5fd732957936c905ee2a6c
haalmarc <marcus-haa@hotmail.com>
2019-11-15 15:26:51 +0100

    Updates

---
+++ precache-manifest.5048854ae30a987b5d43f0a56758e70c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcf70b8de71ac191ac8dbb547fc0f36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc5e592c90f056548d83",
+    "url": "/static/css/main.95388fd5.chunk.css"
+  },
+  {
+    "revision": "331a6c9f26a1f158523b",
+    "url": "/static/js/2.f641515a.chunk.js"
+  },
+  {
+    "revision": "cc5e592c90f056548d83",
+    "url": "/static/js/main.542095ea.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
